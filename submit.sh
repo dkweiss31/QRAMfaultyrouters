ca05f04227f3c64b76ab52efb0012ac1d6cb89bc
Danny <dkweiss@u.northwestern.edu>
2024-02-16 10:32:13 -0500

    fix for calculating eps

--- submit.sh
+++ submit.sh
@@ -2,11 +2,11 @@
 #SBATCH --partition=scavenge
 #SBATCH --job-name=qramfaulty
 #SBATCH -o out/output-%a.txt -e out/errors-%a.txt
-#SBATCH --array=0-99
+#SBATCH --array=0-1999
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=10G
+#SBATCH --mem=1G
 #SBATCH --time=01:00:00
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=daniel.weiss@yale.edu
@@ -15,14 +15,15 @@
 IDX=$SLURM_ARRAY_TASK_ID
 n=5
 
-num_eps=10
-num_instance_scans=10
+# num_eps * num_instance_scans should equal number of array jobs
+num_eps=20
+num_instance_scans=100
 eps_idx=$((SLURM_ARRAY_TASK_ID / num_instance_scans))
 eps_seq=($(seq 1 1 $num_eps))
-eps=$((${eps_seq[$eps_idx]} / 100))
+eps=$(echo "scale=2; ${eps_seq[$eps_idx]} / 100" | bc)
 
 num_instances=1000
-rng_seed=$((IDX * 3567483))
+rng_seed=$((IDX * n * 3567483 + 23784))
 ############################
 
 module load miniconda

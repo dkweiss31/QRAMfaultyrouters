be81371e5788dc01b8149062da6abf6786dde3ef
Danny <dkweiss@u.northwestern.edu>
2024-03-26 19:05:36 -0400

    print different attributes simple reallocation largest functioning subtree AnalyticUnrepair Added back tests in main

--- faulty_routers.py
+++ faulty_routers.py
@@ -19,11 +19,21 @@ class QRAMRouter:
     functioning: bool
         whether or not this router is functioning
     """
-    def __init__(self, location="0b", left_child=None, right_child=None, functioning=True):
+    def __init__(
+        self,
+        location="0b",
+        left_child=None,
+        right_child=None,
+        functioning=True,
+        largest_functioning_subtree_depth=0,
+        part_of_subtree=False,
+    ):
         self.location = location
         self.left_child = left_child
         self.right_child = right_child
         self.functioning = functioning
+        self.largest_functioning_subtree_depth_as_root = largest_functioning_subtree_depth
+        self.part_of_subtree = part_of_subtree
 
     def create_tree(self, tree_depth, current_location="0b"):
         """create a tree of depth tree_depth without deactivating any routers"""
@@ -42,9 +52,9 @@ class QRAMRouter:
         else:
             return self.right_child.tree_depth() + 1
 
-    def print_tree(self, indent="", am_I_a_right_or_left_child="right"):
+    def print_tree(self, indent="", am_I_a_right_or_left_child="right", print_attr="location"):
         if self.right_child:
-            self.right_child.print_tree(indent + "     ", "right")
+            self.right_child.print_tree(indent + "     ", am_I_a_right_or_left_child="right", print_attr=print_attr)
         print(indent, end="")
         if am_I_a_right_or_left_child == "right":
             print("┌───", end="")
@@ -53,11 +63,14 @@ class QRAMRouter:
         else:
             print("    ", end="")
         if self.functioning:
-            print(self.location)
+            if self.part_of_subtree:
+                print(f"{Fore.BLUE}{getattr(self, print_attr)}{Style.RESET_ALL}")
+            else:
+                print(getattr(self, print_attr))
         else:
-            print(f"{Fore.RED}{self.location}{Style.RESET_ALL}")
+            print(f"{Fore.RED}{getattr(self, print_attr)}{Style.RESET_ALL}")
         if self.left_child:
-            self.left_child.print_tree(indent + "     ", "left")
+            self.left_child.print_tree(indent + "     ", am_I_a_right_or_left_child="left", print_attr=print_attr)
 
     def deactivate_self_and_below_routers(self):
         """called if a parent router is not functioning. All below routers
@@ -87,6 +100,88 @@ class QRAMRouter:
                 new_rn_list = self.left_child.fabrication_instance(failure_rate, rn_list=new_rn_list)
             return new_rn_list
 
+    def simple_reallocation(self, m, k):
+        """m is the depth of the QRAM we are after, and k is the level of the QRAM this
+        router should be acting as"""
+        def _simple_reallocation(m_depth, k_depth, router, existing_subtree=None):
+            if existing_subtree is None:
+                existing_subtree = []
+            router.part_of_subtree = True
+            # we've reached the bottom
+            if m_depth - k_depth == 1:
+                existing_subtree.append(router.location)
+                return existing_subtree
+            right_avail_router_list = router.right_child.lowest_router_list_functioning_or_not(functioning=True)
+            left_avail_router_list = router.left_child.lowest_router_list_functioning_or_not(functioning=True)
+            num_right = len(right_avail_router_list)
+            num_left = len(left_avail_router_list)
+            # can succesfully split this router as enough addresses available on each side
+            if num_right >= 2**(m_depth-k_depth-2) and num_left >= 2**(m_depth-k_depth-2):
+                router.part_of_subtree = True
+                existing_subtree = _simple_reallocation(
+                    m_depth, k_depth + 1, router.right_child, existing_subtree=existing_subtree
+                )
+                existing_subtree = _simple_reallocation(
+                    m_depth, k_depth + 1, router.left_child, existing_subtree=existing_subtree
+                )
+                return existing_subtree
+            # check if right or left child router can serve as a level k router
+            elif num_right >= 2**(m_depth - k_depth - 1):
+                existing_subtree = _simple_reallocation(
+                    m_depth, k_depth, router.right_child, existing_subtree=existing_subtree
+                )
+                return existing_subtree
+            elif num_left >= 2**(m_depth - k_depth - 1):
+                existing_subtree = _simple_reallocation(
+                    m_depth, k_depth, router.left_child, existing_subtree=existing_subtree
+                )
+                return existing_subtree
+            else:
+                raise SimpleReallocationFailure
+        try:
+            subtree = _simple_reallocation(m, k, self)
+            return subtree
+        except SimpleReallocationFailure:
+            return None
+
+    def all_functioning_to_depth(self, depth):
+        if depth == 0:  # indicates we were only asking about above routers, doesn't include self
+            return True
+        elif not self.functioning:
+            return False
+        elif self.right_child is None:
+            if depth == 1:
+                return True
+            else:
+                return False
+        else:
+            return (self.right_child.all_functioning_to_depth(depth - 1)
+                    and self.left_child.all_functioning_to_depth(depth - 1))
+
+    def calculate_my_largest_functioning_subtree(self):
+        if not self.functioning:
+            self.largest_functioning_subtree_depth_as_root = 0
+        else:
+            n = self.tree_depth()
+            for depth in range(1, n + 1):  # go until not all functioning to a certain depth
+                depth_functioning_subtree = self.all_functioning_to_depth(depth)
+                if depth_functioning_subtree:
+                    self.largest_functioning_subtree_depth_as_root = depth
+                else:
+                    break
+            if n != 1:
+                self.right_child.calculate_my_largest_functioning_subtree()
+                self.left_child.calculate_my_largest_functioning_subtree()
+
+    def largest_functioning_subtree(self):
+        if self.right_child is None:
+            return self.largest_functioning_subtree_depth_as_root
+        else:
+            return max(self.largest_functioning_subtree_depth_as_root,
+                       self.right_child.largest_functioning_subtree(),
+                       self.left_child.largest_functioning_subtree(),
+                       )
+
     def count_number_faulty_addresses(self):
         if self.right_child is None:
             if self.functioning:
@@ -100,10 +195,11 @@ class QRAMRouter:
                 return (self.right_child.count_number_faulty_addresses()
                         + self.left_child.count_number_faulty_addresses())
 
-    def router_list_functioning_or_not(self, functioning=False, existing_router_list=None):
-        """function that can compute a list of faulty routers and also functioning routers.
-         If functioning is set to False, this creates a list of all non-functioning routers.
-         If functioning is set to true, then it creates a list of functioning routers."""
+    def lowest_router_list_functioning_or_not(self, functioning=False, existing_router_list=None):
+        """function that can compute a list of faulty routers and also functioning routers
+        among those at the bottom of the tree. If functioning is set to False,
+        this creates a list of all non-functioning routers. If functioning is set to
+        true, then it creates a list of functioning routers."""
         if existing_router_list is None:
             existing_router_list = []
         if self.right_child is None:
@@ -113,10 +209,10 @@ class QRAMRouter:
             else:
                 return existing_router_list
         else:
-            existing_router_list = self.right_child.router_list_functioning_or_not(
+            existing_router_list = self.right_child.lowest_router_list_functioning_or_not(
                 functioning=functioning, existing_router_list=existing_router_list
             )
-            existing_router_list = self.left_child.router_list_functioning_or_not(
+            existing_router_list = self.left_child.lowest_router_list_functioning_or_not(
                 functioning=functioning, existing_router_list=existing_router_list
             )
             return existing_router_list
@@ -145,8 +241,8 @@ class QRAMRouter:
         n = self.tree_depth()
         # each has depth n-1
         original_tree, augmented_tree = self.assign_original_and_augmented()
-        faulty_router_list = original_tree.router_list_functioning_or_not(functioning=False)
-        available_router_list = augmented_tree.router_list_functioning_or_not(functioning=True)
+        faulty_router_list = original_tree.lowest_router_list_functioning_or_not(functioning=False)
+        available_router_list = augmented_tree.lowest_router_list_functioning_or_not(functioning=True)
         num_faulty = len(faulty_router_list)
         if num_faulty == 0:
             # no repair necessary
@@ -232,3 +328,61 @@ class MonteCarloRouterInstances:
             return list(map(lambda tree: func(tree), self.trees))
         else:
             raise ValueError("func needs to be a string or a callable")
+
+
+class AnalyticUnrepair:
+    def __init__(self, eps):
+        self.eps = eps
+        self.saved_add_available = {
+            "P_2_0": self.eps + (1 - self.eps) * eps ** 2,
+            "P_2_1": 0.0,
+            "P_2_2": 2.0 * self.eps * (1 - self.eps) ** 2,
+            "P_2_3": 0.0,
+            "P_2_4": (1 - self.eps) ** 3,
+        }
+
+    def prob_ell_available(self, n, ell):
+        if ell < 0 or ell > 2**n:
+            return 0.0
+        if f"P_{n}_{ell}" in self.saved_add_available:
+            return self.saved_add_available[f"P_{n}_{ell}"]
+        P_n_ell = sum((1 - self.eps)
+                      * self.prob_ell_available(n - 1, m)
+                      * self.prob_ell_available(n - 1, ell - m)
+                      for m in range(2 ** (n - 1) + 1))
+        if ell == 0:
+            P_n_ell += self.eps
+        self.saved_add_available[f"P_{n}_{ell}"] = P_n_ell
+        return P_n_ell
+
+    def prob_unrepairable(self, n):
+        return sum(self.prob_ell_available(n, ell) for ell in range(0, 2 ** (n - 1)))
+
+
+class SimpleReallocationFailure(Exception):
+    """Raised when the simple reallocaion fails at runtime"""
+    pass
+
+
+if __name__ == "__main__":
+    NUM_INSTANCES = 10000
+    n = 5
+    EPS = 0.1
+    # 6722222232 gives 5 and 3 configuration (not simply repairable)
+    # 6243254322 gives 6 and 2
+    # 22543268254 fails at the second level: starts off with 6, 4, but then drops to 3, 1 on the right.
+    rng = np.random.default_rng(22543268585425543)  # 27585353
+    RN_LIST = rng.random((NUM_INSTANCES, 2 ** n))
+    MYTREE = QRAMRouter()
+    FULLTREE = MYTREE.create_tree(n)
+    FULLTREE.fabrication_instance(EPS, rn_list=RN_LIST[0])
+    FULLTREE.calculate_my_largest_functioning_subtree()
+    FULLTREE.print_tree()
+    #FULLTREE.print_tree(print_attr="largest_functioning_subtree_depth_as_root")
+    avail_router = FULLTREE.lowest_router_list_functioning_or_not(functioning=True)
+    desired_depth_subQ = 4
+    if len(avail_router) >= 2**(desired_depth_subQ - 1):
+        subtree = FULLTREE.simple_reallocation(4, 0)
+        FULLTREE.print_tree()
+        print(subtree)
+    #print(FULLTREE.largest_functioning_subtree())

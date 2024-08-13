a892ef723d6d0922a407ea50156c45fc067b09a7
Danny <dkweiss@u.northwestern.edu>
2024-08-13 16:45:36 -0400

    updated submit file

--- submit.sh
+++ submit.sh
@@ -3,37 +3,36 @@
 #SBATCH --requeue
 #SBATCH --job-name=qramfaulty
 #SBATCH -o out/output-%a.txt -e out/errors-%a.txt
-#SBATCH --array=0-1999
+#SBATCH --array=0-99
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=1G
-#SBATCH --time=01:00:00
+#SBATCH --cpus-per-task=12
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=08:00:00
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=daniel.weiss@yale.edu
 
 ################################## modify params here
-IDX=$SLURM_ARRAY_TASK_ID
-n=5
+n=12
 top_three_functioning=1
-
 # num_eps * num_instance_scans should equal number of array jobs
 num_eps=10
-num_instance_scans=100
+num_instance_scans=10
 eps_idx=$((SLURM_ARRAY_TASK_ID / num_instance_scans))
 eps_seq=($(seq 1 1 $num_eps))
 eps=$(echo "scale=2; ${eps_seq[$eps_idx]} / 100" | bc)
 
-num_instances=1000
-rng_seed=$((IDX * n * 3567483 + 23784))
+num_instances=100000
+rng_seed=$((IDX * n * 3567483 + 23784 * n + 68493 * IDX + 5627))
 ############################
 
 module load miniconda
 conda activate qramfaultyrouters
 python run_faulty_routers.py \
-  --idx=$IDX \
+  --idx=$SLURM_ARRAY_TASK_ID \
   --n=$n \
   --eps=$eps \
   --num_instances=$num_instances \
   --rng_seed=$rng_seed \
-  --top_three_functioning=$top_three_functioning
+  --top_three_functioning=$top_three_functioning \
+  --num_cpus=$SLURM_CPUS_PER_TASK
